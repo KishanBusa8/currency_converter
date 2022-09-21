a9c57621f69e25db1f9a1d9f9e307418cde81404
Kishan Busa <kishanbusa08@gmail.com>
2022-09-21 13:58:06 +0530

    add all currency

---
+++ lib/currency.dart
@@ -0,0 +1,539 @@
+// all available currency
+class Currency {
+  static const String inchNetwork = "1inch";
+  static const String aave = "aave";
+  static const String ada = "ada";
+  static const String aed = "aed";
+  static const String afn = "afn";
+  static const String algo = "algo";
+  static const String all = "all";
+  static const String amd = "amd";
+  static const String amp = "amp";
+  static const String ang = "ang";
+  static const String aoa = "aoa";
+  static const String ar = "ar";
+  static const String ars = "ars";
+  static const String atom = "atom";
+  static const String aud = "aud";
+  static const String avax = "avax";
+  static const String awg = "awg";
+  static const String axs = "axs";
+  static const String azn = "azn";
+  static const String bam = "bam";
+  static const String bat = "bat";
+  static const String bbd = "bbd";
+  static const String bch = "bch";
+  static const String bdt = "bdt";
+  static const String bgn = "bgn";
+  static const String bhd = "bhd";
+  static const String bif = "bif";
+  static const String bmd = "bmd";
+  static const String bnb = "bnb";
+  static const String bnd = "bnd";
+  static const String bob = "bob";
+  static const String brl = "brl";
+  static const String bsd = "bsd";
+  static const String bsv = "bsv";
+  static const String btc = "btc";
+  static const String btcb = "btcb";
+  static const String btg = "btg";
+  static const String btn = "btn";
+  static const String busd = "busd";
+  static const String bwp = "bwp";
+  static const String byn = "byn";
+  static const String byr = "byr";
+  static const String bzd = "bzd";
+  static const String cad = "cad";
+  static const String cake = "cake";
+  static const String cdf = "cdf";
+  static const String celo = "celo";
+  static const String chf = "chf";
+  static const String chz = "chz";
+  static const String clf = "clf";
+  static const String clp = "clp";
+  static const String cny = "cny";
+  static const String comp = "comp";
+  static const String cop = "cop";
+  static const String crc = "crc";
+  static const String cro = "cro";
+  static const String crv = "crv";
+  static const String cuc = "cuc";
+  static const String cup = "cup";
+  static const String cve = "cve";
+  static const String cvx = "cvx";
+  static const String czk = "czk";
+  static const String dai = "dai";
+  static const String dash = "dash";
+  static const String dcr = "dcr";
+  static const String dfi = "dfi";
+  static const String djf = "djf";
+  static const String dkk = "dkk";
+  static const String doge = "doge";
+  static const String dop = "dop";
+  static const String dot = "dot";
+  static const String dzd = "dzd";
+  static const String egld = "egld";
+  static const String egp = "egp";
+  static const String enj = "enj";
+  static const String eos = "eos";
+  static const String ern = "ern";
+  static const String etb = "etb";
+  static const String etc = "etc";
+  static const String eth = "eth";
+  static const String eur = "eur";
+  static const String fei = "fei";
+  static const String fil = "fil";
+  static const String fjd = "fjd";
+  static const String fkp = "fkp";
+  static const String flow = "flow";
+  static const String frax = "frax";
+  static const String ftm = "ftm";
+  static const String ftt = "ftt";
+  static const String gala = "gala";
+  static const String gbp = "gbp";
+  static const String gel = "gel";
+  static const String ggp = "ggp";
+  static const String ghs = "ghs";
+  static const String gip = "gip";
+  static const String gmd = "gmd";
+  static const String gnf = "gnf";
+  static const String gno = "gno";
+  static const String grt = "grt";
+  static const String gt = "gt";
+  static const String gtq = "gtq";
+  static const String gyd = "gyd";
+  static const String hbar = "hbar";
+  static const String hkd = "hkd";
+  static const String hnt = "hnt";
+  static const String hot = "hot";
+  static const String hrk = "hrk";
+  static const String ht = "ht";
+  static const String htg = "htg";
+  static const String huf = "huf";
+  static const String icp = "icp";
+  static const String idr = "idr";
+  static const String ils = "ils";
+  static const String imp = "imp";
+  static const String inj = "inj";
+  static const String inr = "inr";
+  static const String iqd = "iqd";
+  static const String irr = "irr";
+  static const String isk = "isk";
+  static const String jep = "jep";
+  static const String jmd = "jmd";
+  static const String jod = "jod";
+  static const String jpy = "jpy";
+  static const String kava = "kava";
+  static const String kcs = "kcs";
+  static const String kda = "kda";
+  static const String kes = "kes";
+  static const String kgs = "kgs";
+  static const String khr = "khr";
+  static const String klay = "klay";
+  static const String kmf = "kmf";
+  static const String knc = "knc";
+  static const String kpw = "kpw";
+  static const String krw = "krw";
+  static const String ksm = "ksm";
+  static const String kwd = "kwd";
+  static const String kyd = "kyd";
+  static const String kzt = "kzt";
+  static const String lyd = "lyd";
+  static const String lvl = "lvl";
+  static const String luna = "luna";
+  static const String ltl = "ltl";
+  static const String ltc = "ltc";
+  static const String lsl = "lsl";
+  static const String lrd = "lrd";
+  static const String lrc = "lrc";
+  static const String lkr = "lkr";
+  static const String link = "link";
+  static const String leo = "leo";
+  static const String lbp = "lbp";
+  static const String lak = "lak";
+  static const String mzn = "mzn";
+  static const String myr = "myr";
+  static const String mxn = "mxn";
+  static const String mwk = "mwk";
+  static const String mvr = "mvr";
+  static const String mur = "mur";
+  static const String mro = "mro";
+  static const String mop = "mop";
+  static const String mnt = "mnt";
+  static const String mmk = "mmk";
+  static const String mkr = "mkr";
+  static const String mkd = "mkd";
+  static const String miota = "miota";
+  static const String mina = "mina";
+  static const String mga = "mga";
+  static const String mdl = "mdl";
+  static const String matic = "matic";
+  static const String mana = "mana";
+  static const String mad = "mad";
+  static const String nzd = "nzd";
+  static const String npr = "npr";
+  static const String nok = "nok";
+  static const String nio = "nio";
+  static const String ngn = "ngn";
+  static const String nexo = "nexo";
+  static const String neo = "neo";
+  static const String near = "near";
+  static const String nad = "nad";
+  static const String paxg = "paxg";
+  static const String pab = "pab";
+  static const String one = "one";
+  static const String omr = "omr";
+  static const String okb = "okb";
+  static const String pyg = "pyg";
+  static const String pln = "pln";
+  static const String pkr = "pkr";
+  static const String php = "php";
+  static const String pgk = "pgk";
+  static const String pen = "pen";
+  static const String qtum = "qtum";
+  static const String qnt = "qnt";
+  static const String qar = "qar";
+  static const String rwf = "rwf";
+  static const String rune = "rune";
+  static const String rub = "rub";
+  static const String rsd = "rsd";
+  static const String ron = "ron";
+  static const String sos = "sos";
+  static const String sol = "sol";
+  static const String sll = "sll";
+  static const String shp = "shp";
+  static const String shib = "shib";
+  static const String sgd = "sgd";
+  static const String sek = "sek";
+  static const String sdg = "sdg";
+  static const String scr = "scr";
+  static const String sbd = "sbd";
+  static const String sar = "sar";
+  static const String sand = "sand";
+  static const String szl = "szl";
+  static const String syp = "syp";
+  static const String svc = "svc";
+  static const String stx = "stx";
+  static const String std = "std";
+  static const String srd = "srd";
+  static const String tzs = "tzs";
+  static const String twd = "twd";
+  static const String tusd = "tusd";
+  static const String ttt = "ttt";
+  static const String ttd = "ttd";
+  static const String turkisLira = "try";
+  static const String trx = "trx";
+  static const String top = "top";
+  static const String tnd = "tnd";
+  static const String tmt = "tmt";
+  static const String tjs = "tjs";
+  static const String theta = "theta";
+  static const String thb = "thb";
+  static const String uzs = "uzs";
+  static const String uyu = "uyu";
+  static const String usdt = "usdt";
+  static const String usdp = "usdp";
+  static const String usdc = "usdc";
+  static const String usd = "usd";
+  static const String uni = "uni";
+  static const String ugx = "ugx";
+  static const String uah = "uah";
+  static const String vuv = "vuv";
+  static const String vnd = "vnd";
+  static const String vet = "vet";
+  static const String wst = "wst";
+  static const String wemix = "wemix";
+  static const String wbtc = "wbtc";
+  static const String waves = "waves";
+  static const String xtz = "xtz";
+  static const String xrp = "xrp";
+  static const String xpf = "xpf";
+  static const String xof = "xof";
+  static const String xmr = "xmr";
+  static const String xlm = "xlm";
+  static const String xem = "xem";
+  static const String xec = "xec";
+  static const String xdr = "xdr";
+  static const String xdc = "xdc";
+  static const String xcd = "xcd";
+  static const String xau = "xau";
+  static const String xag = "xag";
+  static const String xaf = "xaf";
+  static const String yer = "yer";
+  static const String zwl = "zwl";
+  static const String zmw = "zmw";
+  static const String zmk = "zmk";
+  static const String zil = "zil";
+  static const String zec = "zec";
+  static const String zar = "zar";
+
+  static const allCurrencyWithCountries = {
+    "1inch": "1inch Network",
+    "aave": "Aave",
+    "ada": "Cardano",
+    "aed": "United Arab Emirates Dirham",
+    "afn": "Afghan afghani",
+    "algo": "Algorand",
+    "all": "Albanian lek",
+    "amd": "Armenian dram",
+    "amp": "Synereo",
+    "ang": "Netherlands Antillean Guilder",
+    "aoa": "Angolan kwanza",
+    "ar": "Arweave",
+    "ars": "Argentine peso",
+    "atom": "Atomic Coin",
+    "aud": "Australian dollar",
+    "avax": "Avalanche",
+    "awg": "Aruban florin",
+    "axs": "AXS",
+    "azn": "Azerbaijani manat",
+    "bam": "Bosnia-Herzegovina Convertible Mark",
+    "bat": "Basic Attention Token",
+    "bbd": "Bajan dollar",
+    "bch": "Bitcoin Cash",
+    "bdt": "Bangladeshi taka",
+    "bgn": "Bulgarian lev",
+    "bhd": "Bahraini dinar",
+    "bif": "Burundian Franc",
+    "bmd": "Bermudan dollar",
+    "bnb": "Binance Coin",
+    "bnd": "Brunei dollar",
+    "bob": "Bolivian boliviano",
+    "brl": "Brazilian real",
+    "bsd": "Bahamian dollar",
+    "bsv": "Bitcoin SV",
+    "btc": "Bitcoin",
+    "btcb": "Bitcoin BEP2",
+    "btg": "Bitcoin Gold",
+    "btn": "Bhutan currency",
+    "busd": "Binance USD",
+    "bwp": "Botswanan Pula",
+    "byn": "New Belarusian Ruble",
+    "byr": "Belarusian Ruble",
+    "bzd": "Belize dollar",
+    "cad": "Canadian dollar",
+    "cake": "PancakeSwap",
+    "cdf": "Congolese franc",
+    "celo": "Celo",
+    "chf": "Swiss franc",
+    "chz": "Chiliz",
+    "clf": "Chilean Unit of Account (UF)",
+    "clp": "Chilean peso",
+    "cny": "Chinese Yuan",
+    "comp": "Compound Coin",
+    "cop": "Colombian peso",
+    "crc": "Costa Rican Colón",
+    "cro": "Crypto.com Chain Token",
+    "crv": "Cravy",
+    "cuc": "Cuban peso",
+    "cup": "Cuban Peso",
+    "cve": "Cape Verdean escudo",
+    "cvx": "Convex Finance",
+    "czk": "Czech koruna",
+    "dai": "Dai",
+    "dash": "Dash",
+    "dcr": "Decred",
+    "dfi": "DfiStarter",
+    "djf": "Djiboutian franc",
+    "dkk": "Danish krone",
+    "doge": "Dogecoin",
+    "dop": "Dominican peso",
+    "dot": "Dotcoin",
+    "dzd": "Algerian dinar",
+    "egld": "Elrond",
+    "egp": "Egyptian pound",
+    "enj": "Enjin Coin",
+    "eos": "EOS",
+    "ern": "Eritrean nakfa",
+    "etb": "Ethiopian birr",
+    "etc": "Ethereum Classic",
+    "eth": "Ether",
+    "eur": "Euro",
+    "fei": "Fei USD",
+    "fil": "FileCoin",
+    "fjd": "Fijian dollar",
+    "fkp": "Falkland Islands pound",
+    "flow": "Flow",
+    "frax": "Frax",
+    "ftm": "Fantom",
+    "ftt": "FarmaTrust",
+    "gala": "Gala",
+    "gbp": "Pound sterling",
+    "gel": "Georgian lari",
+    "ggp": "GGPro",
+    "ghs": "Ghanaian cedi",
+    "gip": "Gibraltar pound",
+    "gmd": "Gambian dalasi",
+    "gnf": "Guinean franc",
+    "gno": "Gnosis",
+    "grt": "Golden Ratio Token",
+    "gt": "GateToken",
+    "gtq": "Guatemalan quetzal",
+    "gyd": "Guyanaese Dollar",
+    "hbar": "Hedera",
+    "hkd": "Hong Kong dollar",
+    "hnl": "Honduran lempira",
+    "hnt": "Helium",
+    "hot": "Hydro Protocol",
+    "hrk": "Croatian kuna",
+    "ht": "Huobi Token",
+    "htg": "Haitian gourde",
+    "huf": "Hungarian forint",
+    "icp": "Internet Computer",
+    "idr": "Indonesian rupiah",
+    "ils": "Israeli New Shekel",
+    "imp": "CoinIMP",
+    "inj": "Injective",
+    "inr": "Indian rupee",
+    "iqd": "Iraqi dinar",
+    "irr": "Iranian rial",
+    "isk": "Icelandic króna",
+    "jep": "Jersey Pound",
+    "jmd": "Jamaican dollar",
+    "jod": "Jordanian dinar",
+    "jpy": "Japanese yen",
+    "kava": "Kava",
+    "kcs": "Kucoin",
+    "kda": "Kadena",
+    "kes": "Kenyan shilling",
+    "kgs": "Kyrgystani Som",
+    "khr": "Cambodian riel",
+    "klay": "Klaytn",
+    "kmf": "Comorian franc",
+    "knc": "Kyber Network",
+    "kpw": "North Korean won",
+    "krw": "South Korean won",
+    "ksm": "Kusama",
+    "kwd": "Kuwaiti dinar",
+    "kyd": "Cayman Islands dollar",
+    "kzt": "Kazakhstani tenge",
+    "lak": "Laotian Kip",
+    "lbp": "Lebanese pound",
+    "leo": "LEOcoin",
+    "link": "ChainLink",
+    "lkr": "Sri Lankan rupee",
+    "lrc": "Loopring",
+    "lrd": "Liberian dollar",
+    "lsl": "Lesotho loti",
+    "ltc": "Litecoin",
+    "ltl": "Lithuanian litas",
+    "luna": "Luna Coin",
+    "lvl": "Latvian lats",
+    "lyd": "Libyan dinar",
+    "mad": "Moroccan dirham",
+    "mana": "Decentraland",
+    "matic": "Polygon",
+    "mdl": "Moldovan leu",
+    "mga": "Malagasy ariary",
+    "mina": "Mina",
+    "miota": "IOTA",
+    "mkd": "Macedonian denar",
+    "mkr": "Maker",
+    "mmk": "Myanmar Kyat",
+    "mnt": "Mongolian tugrik",
+    "mop": "Macanese pataca",
+    "mro": "Mauritanian ouguiya",
+    "mur": "Mauritian rupee",
+    "mvr": "Maldivian rufiyaa",
+    "mwk": "Malawian kwacha",
+    "mxn": "Mexican peso",
+    "myr": "Malaysian ringgit",
+    "mzn": "Mozambican Metical",
+    "nad": "Namibian dollar",
+    "near": "NEAR Protocol",
+    "neo": "NEO",
+    "nexo": "NEXO",
+    "ngn": "Nigerian naira",
+    "nio": "Nicaraguan córdoba",
+    "nok": "Norwegian krone",
+    "npr": "Nepalese rupee",
+    "nzd": "New Zealand dollar",
+    "okb": "Okex",
+    "omr": "Omani rial",
+    "one": "Menlo One",
+    "pab": "Panamanian balboa",
+    "paxg": "PAX Gold",
+    "pen": "Sol",
+    "pgk": "Papua New Guinean kina",
+    "php": "Philippine peso",
+    "pkr": "Pakistani rupee",
+    "pln": "Poland złoty",
+    "pyg": "Paraguayan guarani",
+    "qar": "Qatari Rial",
+    "qnt": "Quant",
+    "qtum": "QTUM",
+    "ron": "Romanian leu",
+    "rsd": "Serbian dinar",
+    "rub": "Russian ruble",
+    "rune": "THORChain (ERC20)",
+    "rwf": "Rwandan Franc",
+    "sand": "BeachCoin",
+    "sar": "Saudi riyal",
+    "sbd": "Solomon Islands dollar",
+    "scr": "Seychellois rupee",
+    "sdg": "Sudanese pound",
+    "sek": "Swedish krona",
+    "sgd": "Singapore dollar",
+    "shib": "Shiba Inu",
+    "shp": "Saint Helena pound",
+    "sll": "Sierra Leonean leone",
+    "sol": "Sola",
+    "sos": "Somali shilling",
+    "srd": "Surinamese dollar",
+    "std": "São Tomé and Príncipe Dobra (pre-2018)",
+    "stx": "Stox",
+    "svc": "Salvadoran Colón",
+    "syp": "Syrian pound",
+    "szl": "Swazi lilangeni",
+    "thb": "Thai baht",
+    "theta": "Theta",
+    "tjs": "Tajikistani somoni",
+    "tmt": "Turkmenistani manat",
+    "tnd": "Tunisian dinar",
+    "top": "Tongan Paʻanga",
+    "trx": "TRON",
+    "try": "Turkish lira",
+    "ttd": "Trinidad & Tobago Dollar",
+    "ttt": "Tap Project",
+    "tusd": "True USD",
+    "twd": "New Taiwan dollar",
+    "tzs": "Tanzanian shilling",
+    "uah": "Ukrainian hryvnia",
+    "ugx": "Ugandan shilling",
+    "uni": "Universe",
+    "usd": "United States dollar",
+    "usdc": "USD Coin",
+    "usdp": "USDP Stablecoin",
+    "usdt": "Tether",
+    "uyu": "Uruguayan peso",
+    "uzs": "Uzbekistani som",
+    "vet": "Vechain",
+    "vnd": "Vietnamese dong",
+    "vuv": "Vanuatu vatu",
+    "waves": "Waves",
+    "wbtc": "Wrapped Bitcoin",
+    "wemix": "WEMIX",
+    "wst": "Samoan tala",
+    "xaf": "Central African CFA franc",
+    "xag": "Silver Ounce",
+    "xau": "XauCoin",
+    "xcd": "East Caribbean dollar",
+    "xdc": "XDC Network",
+    "xdr": "Special Drawing Rights",
+    "xec": "Eternal Coin",
+    "xem": "NEM",
+    "xlm": "Stellar",
+    "xmr": "Monero",
+    "xof": "West African CFA franc",
+    "xpf": "CFP franc",
+    "xrp": "XRP",
+    "xtz": "Tezos",
+    "yer": "Yemeni rial",
+    "zar": "South African rand",
+    "zec": "ZCash",
+    "zil": "Zilliqa",
+    "zmk": "Zambian kwacha",
+    "zmw": "Zambian Kwacha",
+    "zwl": "Zimbabwean Dollar"
+  };
+}
